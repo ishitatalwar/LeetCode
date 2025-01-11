a496ec5c3953d1969d31f50eae581122feec0206
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2025-01-11 22:36:10 +0530

    Time: 0 ms (100%), Space: 8.3 MB (73.71%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -4,6 +4,7 @@ public:
     int minAddToMakeValid(string s) {
         int opening = 0; 
         int overbalanced_closing = 0;
+        // checks for the whether the first character is not a null character. 
         for(int i = 0;s[i]!='\0'; i++){
             if(s[i]=='(') opening++;
             else if(opening>0) opening--;
