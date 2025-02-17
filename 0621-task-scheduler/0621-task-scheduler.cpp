082f9ac51f4870dcd82b9a4933505ae7be24d14a
Ishita Talwar <103492839+ishitatalwar@users.noreply.github.com>
2025-02-17 20:36:06 +0530

    Time: 0 ms (100%), Space: 38.1 MB (99.53%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int p = tasks.size();
+
+        if(n==0){
+            return p;
+        }
+        int counter[26] = {0};
+        for (char &ch : tasks){
+            counter[ch - 'A']++;
+        }
+        sort(begin(counter), end(counter));
+        int chunks = counter[25] - 1;
+        int idleS = chunks * n;
+        for(int i = 24; i>=0; i--){
+            idleS -= min(chunks, counter[i]);
+        }
+        if(idleS > 0){
+            return p + idleS;
+        }
+        return p;
+    }
+};
